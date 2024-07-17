a4e5b0c6a6f6cfec9f06f65d65329e359dded13f
ManuelDeV-exe <75885687+ManuelDeV-exe@users.noreply.github.com>
2024-07-17 16:16:29 +0200

    v0.1

--- __init__.py
+++ __init__.py
@@ -1,6 +1,29 @@
 import FreeCAD
 import FreeCADGui
 import TechDraw
+import os
 
-print('begin alles klar')
-FreeCADGui.activateWorkbench("TechDrawWorkbench")
+class Meins:
+    def GetResources(self):
+        mypath = os.path.dirname(__file__)
+        return {
+             'Pixmap': mypath + "/Icons/QuickMeasure.svg",
+             'MenuText': 'test Measure',
+             'ToolTip': 'test'
+             }
+
+    def Activated(self, placeholder = None):
+        print('Activated')
+
+    def Deactivated(self):
+        print('Deactivated')
+
+    def IsEnabled(self):
+        print('IsEnabled')
+        return()
+
+    def IsActive(self):
+        print('IsActive')
+        return(True)
+    
+FreeCADGui.addCommand('TechDrawWorkbench', Meins())
